c5d45541e25936b153e17ea4f6de91b7442da215
Yosip Mike Colin <colinparrado@gmail.com>
2024-11-27 20:26:15 -0500

    Project structuring

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from "./App";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
